9154a01b9c32f3f6c9b4932b2a73d795138ae79a
borAn77 <borangedik123@gmail.com>
2026-05-21 19:12:02 +0200

    fix: FRONTEND_URL www ile düzelt

--- mobile/config.ts
+++ mobile/config.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'https://randezy-api.vercel.app';
-export const FRONTEND_URL = 'https://randezy.com';
+export const FRONTEND_URL = 'https://www.randezy.com';
 export const SUPABASE_URL = 'https://gaxjfcwdwefdmwpzskfl.supabase.co';
 export const SUPABASE_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdheGpmY3dkd2VmZG13cHpza2ZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMTAwODMsImV4cCI6MjA5MzU4NjA4M30.ADmbji7P3hgIYV4knVsd-VZx_FcIOGT6la26N3fvx5o';
